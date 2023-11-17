1300026b1d2709c4b14c02b9e5c6114eae41c86a
Andriikryl <andreyk557@gmail.com>
2023-11-17 15:53:20 +0200

    add header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Container } from "../container/Container";
+import styles from "./style.module.css";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <Container>
+        <div className={styles.header__box}>
+          <nav className={styles.nav}>
+            <ul role="list" className={styles.list}>
+              <li className={styles.list__item}>
+                <Link href={"/"} className={styles.item__link}>
+                  Home
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </Container>
+    </header>
+  );
+}
