753e876aaadca5824cc97f1acc6fa350cd2c1d47
Andriikryl <andreyk557@gmail.com>
2023-11-17 20:02:16 +0200

    add fadeOut component

---
+++ src/components/FadeOut/FadeOut.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import style from "./style.module.css";
+export default function FadeOut() {
+    return <div className={style.box}></div>;
+}
--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -3,6 +3,7 @@ import styles from "./style.module.css";
 import { Container } from "../container/Container";
 import AnimationWrapper from "../animationWrapper/AnimationWrapper";
 import FadiIn from "../FadeIn/FadiIn";
+import FadeOut from "../FadeOut/FadeOut";
 export default function Gallery() {
   return (
     <section className={styles.gallery}>
@@ -10,6 +11,9 @@ export default function Gallery() {
         <div className={styles.gallery__box}>
           <AnimationWrapper nameLink={"fade in"} href="/fade-in">
             <FadiIn />
+          </AnimationWrapper>
+          <AnimationWrapper nameLink={"fade out"} href="/fade-out">
+            <FadeOut />
           </AnimationWrapper>
         </div>
       </Container>
