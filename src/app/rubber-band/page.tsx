174e731ceb28ef16a964f60f2662d4e0d0d2101b
Andriikryl <andreyk557@gmail.com>
2023-11-19 15:00:37 +0200

    add ruberrBandP page

---
+++ src/app/rubber-band/page.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import PageWrapper from "@/components/pageWrapper/PageWrapper";
+import { Container } from "@/components/container/Container";
+import Code from "@/components/code/Code";
+import RubberBand from "@/components/RubberBand/RubberBand";
+
+export default function RubberBandP() {
+  return (
+    <section>
+      <Container>
+        <PageWrapper title="Side in out">
+          <RubberBand />
+        </PageWrapper>
+        <Code
+          code=".box:hover{
+            animation: fade-in 1s ease-in-out;
+        }
+        
+        @keyframes fade-in {
+            0%{
+                transform:  scale(1);
+              }
+              30%{
+                transform:  scale(1.25);
+              }
+              40%{
+                transform:  scale(0.75);
+              }
+              50%{
+                transform:  scale(1.15);
+              }
+              65%{
+                transform:  scale(0.95);
+              }
+              75% {
+                transform: scale(1.05);
+              }
+              100% {
+                transform: scale(1);
+              }
+        }"
+        />
+      </Container>
+    </section>
+  );
+}
